f327c5465ffda5c76496c054761941e1e73af950
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-01 17:34:56 +0200

    FrontEndMentor Practice

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import "../src/App.css"
+
+
+function App() {
+
+return(
+ <div className='bigmain' >
+  <div className='main'>
+      <img className='qrCodeImg' src='./src/assets/image-qr-code.png'></img>
+      <h2 className='title-qr'>Improve your front-end skills by building projects</h2>
+      <p className='text-qr'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+  </div>
+  </div> 
+)
+}
+
+export default App
