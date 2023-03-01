a580e1d6537b45c9486fb5969e89aca5158a517b
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-01 21:01:01 +0200

    img web

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 return(
  <div className='bigmain' >
   <div className='main'>
-      <img className='qrCodeImg' src='./src/assets/image-qr-code.png'></img>
+      <img className='qrCodeImg' src='https://user-images.githubusercontent.com/98059798/222238370-2f27c263-7762-4b5d-8d22-a4344e469edc.png'></img>
       <h2 className='title-qr'>Improve your front-end skills by building projects</h2>
       <p className='text-qr'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
   </div>
